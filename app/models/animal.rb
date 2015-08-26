bd11ae731c5c6cee31b6f3f3a00ff60caa2b4db2
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2015-08-26 01:33:04 -0300

    added custom thumbnail css class to fix image size

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,7 @@
 class Animal < ActiveRecord::Base
 	enum family: [ :gato, :cachorro ]
 	enum ageunit: [ :mes, :ano ]
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "64x64#" }
+	has_attached_file :image, :styles => { :medium => "256x256>", :thumb => "64x64#" }
 	belongs_to :user
 
 
